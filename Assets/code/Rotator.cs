6dab15ea02303eeefb120e0c599bc6236e2e75b5
Cesar Francisco Chavez Maldonado <cesarchamal@gmail.com>
2020-05-12 00:13:29 +0200

    Upload code for UnitySourceAssets project.

---
+++ Assets/code/Rotator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	
+	public Transform target;
+	bool toggler;
+	Vector3 random_pos;
+	
+	void Update()
+	{
+		bool space = Input.GetKeyDown(KeyCode.Space);
+		bool r = Input.GetKeyDown(KeyCode.R);
+		
+		if (space)
+		{
+			transform.rotation = Quaternion.identity;
+			toggler = !toggler;
+		}
+		
+		if (r)
+		{
+			random_pos = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10));
+		}
+		
+		if (!toggler)
+		{
+			transform.rotation = Quaternion.LookRotation(target.position);
+		}
+		
+		target.position = Vector3.Slerp(target.position, random_pos, Time.deltaTime);
+		
+	}
+	
+}
